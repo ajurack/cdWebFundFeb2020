4b9b2197138bf95de1aa59c257ecdec0e3b4c8cb
Anne <anne.jurack@gmail.com>
2020-02-17 14:03:23 -0600

    initial commit

---
+++ hello_world/hello.js
@@ -0,0 +1,11 @@
+var passPhrase = "Hello World";
+
+if(passPhrase=="Open Sesame") {
+    console.log("Wrong Secret Passphrase");
+} else if (passPhrase == "Open up come on") {
+    console.log("nuh uh");
+} else if (passPhrase == "Hello World") {
+    console.log("Success!!!")
+} else {
+    console.log("Not even close");
+}  
